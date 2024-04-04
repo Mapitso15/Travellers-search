71eda551693add5dfb719c81d9debd9397393f25
Mamello <mmoshoeshoe@webelieve.co.za>
2024-04-04 22:45:59 +0200

    add widgets placeholders

---
+++ src/app/shared/widgets/itinerary-form/itinerary-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itinerary-form',
+  templateUrl: './itinerary-form.component.html',
+  styleUrls: ['./itinerary-form.component.scss']
+})
+export class ItineraryFormComponent {
+
+}
