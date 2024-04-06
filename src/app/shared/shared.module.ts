1cf66a653313ba8baac1e3e348471551766efd40
Mamello <mmoshoeshoe@webelieve.co.za>
2024-04-06 17:13:53 +0200

    add logo, slogan to topbar | style top bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { WidgetsModule} from './widgets/widgets.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
+    WidgetsModule,
+    BrowserModule 
+  ],
+  exports: [
     WidgetsModule
   ],
   providers: [],
