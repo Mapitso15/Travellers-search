7fd1f65d2554cb8ddd3471d20c66e6cab69156a0
Mamello <mmoshoeshoe@webelieve.co.za>
2024-04-07 16:43:12 +0200

    add form module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { WidgetsModule} from './widgets/widgets.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     WidgetsModule,
-    BrowserModule 
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   exports: [
-    WidgetsModule
+    WidgetsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
 })
